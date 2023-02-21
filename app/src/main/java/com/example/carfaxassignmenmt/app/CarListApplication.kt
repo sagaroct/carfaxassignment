d42eb976edd51a3126c05a795f8b975fae02f6ca
Sagar Pujari <sagar.pujari@inmobi.com>
2023-02-21 13:39:59 +0530

    Image cache issue fixed.

--- app/src/main/java/com/example/carfaxassignmenmt/app/CarListApplication.kt
+++ app/src/main/java/com/example/carfaxassignmenmt/app/CarListApplication.kt
@@ -1,19 +1,27 @@
-package com.example.carfaxassignmenmt
+package com.example.carfaxassignmenmt.app
 
 import android.app.Application
 import android.content.Context
+import coil.ImageLoader
+import coil.ImageLoaderFactory
 import dagger.hilt.android.HiltAndroidApp
 
 /**
  * Created by sagar on 4/8/17.
  */
 @HiltAndroidApp
-class CarListApplication : Application() {
+class CarListApplication : Application(), ImageLoaderFactory {
 
     companion object {
         operator fun get(context: Context?): CarListApplication {
             return context?.applicationContext as CarListApplication
         }
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .respectCacheHeaders(false)
+            .build()
     }
 
 }
